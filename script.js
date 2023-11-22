29f5d04e55c7852852296ed7d303e71ea08b5e33
johann219 <147496124+johann219@users.noreply.github.com>
2023-11-22 21:10:13 +0300

    Implement random computer choice

---
+++ script.js
@@ -0,0 +1,16 @@
+function getComputerChoice() {
+  let random = Math.floor(Math.random() * 3 + 1);
+  let computerChoice = "";
+  switch (random) {
+    case 1: 
+      computerChoice = "Rock";
+      break;
+    case 2:
+      computerChoice = "Paper";
+      break;
+    case 3: 
+      computerChoice = "Scissors";
+      break;
+  }
+  return computerChoice;
+}
