3ba2b10ff30526656705d64a0ca1eb290d160ba1
johann219 <147496124+johann219@users.noreply.github.com>
2024-02-23 17:40:42 +0300

    Add function to return string with capitalized first letter

--- script.js
+++ script.js
@@ -10,3 +10,9 @@ const getComputerChoice = () => {
             return 'scissors';
     }
 };
+
+const capitalizeString = (string) => {
+    const firstLetter = string.slice(0, 1).toUpperCase();
+    string = firstLetter + string.slice(1).toLowerCase();
+    return string;
+}
